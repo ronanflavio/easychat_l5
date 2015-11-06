3011fbfd13ea9d0b978700bef5191c514809d432
Ronan Flávio Ribeiro <ronan.ribeiro@esportes.mg.gov.br>
2015-11-06 11:30:15 -0200

    fix: corrigindo caminho de publicação de arquivos

--- src/EasychatServiceProvider.php
+++ src/EasychatServiceProvider.php
@@ -21,15 +21,10 @@ class EasychatServiceProvider extends ServiceProvider
         // define the path for the view files
         $this->loadViewsFrom(__DIR__ . '/../views', 'easychat');
 
-        // define the files that will be publisheds
+        // define the paths that will be publisheds
         $this->publishes([
-            // __DIR__.'/config/config.php' => config_path('packages/Ronanflavio/Easychat/config.php'),
-            // __DIR__.'/config/tables.php' => config_path('packages/Ronanflavio/Easychat/tables.php'),
-            // __DIR__.'/migrations/2015_11_03_000000_create_ec_rooms_table.php' => config_path('packages/Ronanflavio/Easychat/2015_11_03_000000_create_ec_rooms_table.php'),
-            // __DIR__.'/migrations/2015_11_03_000000_create_ec_server_messages_table.php' => config_path('packages/Ronanflavio/Easychat/2015_11_03_000000_create_ec_server_messages_table.php'),
-            // __DIR__.'/migrations/2015_11_03_000000_create_ec_user_messages_table.php' => config_path('packages/Ronanflavio/Easychat/2015_11_03_000000_create_ec_user_messages_table.php'),
             __DIR__.'/config' => config_path('packages/Ronanflavio/Easychat'),
-            // __DIR__.'/migrations' => config_path('packages/Ronanflavio/Easychat'),
+            __DIR__.'/migrations' => base_path('migrations/Ronanflavio/Easychat'),
             __DIR__.'/../public' => public_path('packages/Ronanflavio/Easychat'),
         ]);
     }
